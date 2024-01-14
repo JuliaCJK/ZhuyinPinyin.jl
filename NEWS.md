a297376cc1b164f28f437f722850270e77397950
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-01-14 18:16:52 -0500

    feat: add pron strings

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# NEWS/CHANGELOG
+
+## v1.1.0
+
+- added `pinyin` and `zhuyin` string macros to make creating `Pinyin` and `Zhuyin` structs easier
--- src/pronunciations.jl
+++ src/pronunciations.jl
@@ -3,7 +3,15 @@ struct Zhuyin{T <: AbstractString}
     contents::T
 end
 
+macro zhuyin_str(str)
+    Zhuyin(str)
+end
+
 struct Pinyin{T <: AbstractString}
     contents::T
+end
+
+macro pinyin_str(str)
+    Pinyin(str)
 end
 
